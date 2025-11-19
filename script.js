81ba90afea6f0c9f91b3cd9b201ba23b64913b9c
Alex Marfo Appiah <alexmarfo509@gmail.com>
2025-11-19 12:07:04 +0100

    Refactor AJAX calls to use renderCountry function

--- script.js
+++ script.js
@@ -11,19 +11,41 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 //CREATING AJAX CALLS (OLD WAY - XMLHTTP REQUEST FUNCTION)
-const getCountryData = function (country) {
-    const request = new XMLHttpRequest()
-    //URL to make AJAX call using GET - Background activity(asynchronous)
-    request.open("GET", `https://restcountries.com/v2/name/${country}`)
-    request.send()
-    //registering callback on request object
-    request.addEventListener("load", function () {
-        //converting JSON data into an object
-        const [data] = JSON.parse(this.responseText)
-        console.log(data)
+// const getCountryData = function (country) {
+//     const request = new XMLHttpRequest()
+//     //URL to make AJAX call using GET - Background activity(asynchronous)
+//     request.open("GET", `https://restcountries.com/v2/name/${country}`)
+//     request.send()
+//     //registering callback on request object
+//     request.addEventListener("load", function () {
+//         //converting JSON data into an object
+//         const [data] = JSON.parse(this.responseText)
+//         console.log(data)
 
-        //building component
-        const html = `
+//         //building component
+//         const html = `
+//     <article class="country">
+//         <img class="country__img" src="${data.flag}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${+(data.population / 1000000).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//           </div>
+//     </article>
+//         `
+//         //displaying HTML content
+//         countriesContainer.insertAdjacentHTML("beforeend", html)
+//         //updating opacity style
+//         countriesContainer.style.opacity = 1
+//     })
+// };
+
+//Exporting data into its own functionality
+const renderCountry = function(data) {
+    //Exporting data into its own functionality
+    const html = `
     <article class="country">
         <img class="country__img" src="${data.flag}" />
           <div class="country__data">
@@ -40,11 +62,50 @@ const getCountryData = function (country) {
         //updating opacity style
         countriesContainer.style.opacity = 1
     })
+}
+
+const getCountryAndNeighbour = function (country) {
+    //AJAX call for first country
+    const request = new XMLHttpRequest()
+    //URL to make AJAX call using GET - Background activity(asynchronous)
+    request.open("GET", `https://restcountries.com/v2/name/${country}`)
+    request.send()
+    //registering callback on request object
+    request.addEventListener("load", function () {
+        //converting JSON data into an object
+        const [data] = JSON.parse(this.responseText)
+        console.log(data)
+        
+        //Rendering first country
+        renderCountry(data)
+
+        //Getting neighbour country info
+        const [neighbour] = data.borders?.[0]
+
+        if(!neighbour) return;
+
+        //AJAX call for second country
+        const requestTwo = new XMLHttpRequest()
+        //URL to make AJAX call using GET - Background activity(asynchronous)
+        requesTwo.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`)
+        requestTwo.send()
+
+        requestTwo.addEventListener("load", function () {
+        //converting JSON data into an object
+        const dataTwo = JSON.parse(this.responseText)
+            console.log(dataTwo)
+
+            renderCountry(dataTwo)
+        };
+    };
 };
+
+
 //SIMULATANEOUS AJAX CALLS
-getCountryData("portugal")
-getCountryData("ghana")
-getCountryData("nigeria")
+getCountryAndNeighbour("ghana")
+// getCountryAndNeighbour("nigeria")
+// getCountryAndNeighbour("egypt")
+
 
 
 
